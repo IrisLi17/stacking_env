2683fc20a33611915d0c7775a942539d40bb7cbd
Yunfei Li <yunfeili.cloud@gmail.com>
2022-04-15 10:55:12 +0800

    Register pick and place env

--- env/__init__.py
+++ env/__init__.py
@@ -1,5 +1,5 @@
 import gym
-from env.robot_arm_env import ArmStack
+from env.robot_arm_env import ArmStack, ArmPickAndPlace
 
 
 gym.register(
@@ -7,5 +7,12 @@ gym.register(
     kwargs=dict(
         robot="panda", reward_type="sparse", n_object=6,
         n_to_stack=[1, 2, 3, 4, 5, 6], action_dim=4,
+    )
+)
+
+gym.register(
+    "BulletPickAndPlace-v1", entry_point=ArmPickAndPlace, max_episode_steps=100,
+    kwargs=dict(
+        robot="panda", reward_type="sparse", n_object=6, action_dim=4
     )
 )
